286ece1d2533caec6a317758499921de3009bc54
Gerseneck <wang14240@gmail.com>
2026-07-24 11:23:33 -0400

    feat: better looking theme toggle button

--- assets/js/pre.js
+++ assets/js/pre.js
@@ -1,17 +1,20 @@
 const dark_mode_preference = window.matchMedia("(prefers-color-scheme: dark)");
 
 const get_theme = () => {
-    system_defaults = dark_mode_preference.matches ? "dark" : "light";
+    let system_defaults = dark_mode_preference.matches ? "dark" : "light";
 
     return localStorage.getItem("data-bs-theme") ?? system_defaults;
-}
+};
 
 const set_mode = (dark) => {
-    document.documentElement.setAttribute("data-bs-theme", dark ? "dark" : "light");
-}
+    let new_state = dark ? "dark" : "light";
+
+    localStorage.setItem("data-bs-theme", new_state);
+    document.documentElement.setAttribute("data-bs-theme", new_state);
+};
 
 dark_mode_preference.addEventListener("change", (event) => {
     set_mode(event.matches);
-})
+});
 
 addEventListener("DOMContentLoaded", document.documentElement.setAttribute("data-bs-theme", get_theme()));
